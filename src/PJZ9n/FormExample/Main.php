4edbdc52b5521524b7863d2ada65093df4840449
PJZ9n <pjz9n@yahoo.co.jp>
2020-05-28 10:17:13 +0900

    テンプレート書き換え

--- src/PJZ9n/FormExample/Main.php
+++ src/PJZ9n/FormExample/Main.php
@@ -3,31 +3,29 @@
 /**
  * Copyright (c) 2020 PJZ9n.
  *
- * This file is part of PluginTemplate.
+ * This file is part of FormExample.
  *
- * PluginTemplate is free software: you can redistribute it and/or modify
+ * FormExample is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * PluginTemplate is distributed in the hope that it will be useful,
+ * FormExample is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PluginTemplate.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FormExample. If not, see <http://www.gnu.org/licenses/>.
  */
 
 declare(strict_types=1);
 
-namespace PJZ9n\Template;
+namespace PJZ9n\FormExample;
 
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
-    
     //
-    
 }
